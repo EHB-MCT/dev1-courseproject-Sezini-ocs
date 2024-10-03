088f950d709d13ef453304bdcec21adce27bbf1f
Jean Bonheur <jean-bonheur.sezini@student.ehb.be>
2024-10-03 13:17:11 +0200

    done spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,47 @@
 "use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+drawLine();
+
+function drawLine()
+
+{   
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(50,50,300,300);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle="#00FF00"
+    context.rect(125,125,150,50);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle="#00FF00"
+    context.rect(175,175,50,50);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle="#00FF00"
+    context.rect(75,225,100,100);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle="#00FF00"
+    context.rect(225,225,100,100);
+    context.fill(); 
+
+    context.beginPath();
+    context.fillStyle="#00FF00"
+    context.rect(175,275,50,50);
+    context.fill(); 
+
+
+
+}
+
+
+
